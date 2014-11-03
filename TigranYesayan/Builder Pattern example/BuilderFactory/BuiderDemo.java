ae0cbae4bcc93efd8ce9c0297d692347a57b5469
TigranYesayan <tigranyessayan@gmail.com>
2014-11-03 16:43:06 +0400

    Change Demo

--- TigranYesayan/Builder Pattern example/BuilderFactory/BuiderDemo.java
+++ TigranYesayan/Builder Pattern example/BuilderFactory/BuiderDemo.java
@@ -3,14 +3,23 @@ package com.synisys.trainings.BuilderFactory;
 public class BuiderDemo {
 
 	public static void main(String[] args) {
+		try{
 		Contact user1 = new Contact.ContactBuilder("FirstName", "LastName","firstName.lastName@mail.mm").phoneNumber("555 000 000 000")
 				.organisation("MGM")
 				.build();
 		System.out.println(user1);
+		}
+		catch(IllegalArgumentException e){
+			e.printStackTrace();
+		} 
 
-		Contact user2 = new Contact.ContactBuilder("FirstName", "LastName","firstName.lastName@mail.mm").phoneNumber("555 000 000 000").build();
+		try{
+		Contact user2 = new Contact.ContactBuilder("FirstName", "LastName","firstName.lastName@mailmm").phoneNumber("555 000 000 000").build();
 		System.out.println(user2);
-
+		}
+		catch(IllegalArgumentException e){
+			e.printStackTrace();
+		} 
 		try{
 			Contact user3 = new Contact.ContactBuilder("FirstName", "LastName","firstName.lastName@mail.mm").build();
 			System.out.println(user3);

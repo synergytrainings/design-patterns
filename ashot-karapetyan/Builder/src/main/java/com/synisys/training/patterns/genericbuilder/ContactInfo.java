24ee45c07e150d4104f1ea8359e7295d3c9e7ae8
Ashot Karapetyan <karapetyan.ash@gmail.com>
2014-11-04 04:19:08 +0400

    Homework for Builder pattern.

--- ashot-karapetyan/Builder/src/main/java/com/synisys/training/patterns/genericbuilder/ContactInfo.java
+++ ashot-karapetyan/Builder/src/main/java/com/synisys/training/patterns/genericbuilder/ContactInfo.java
@@ -31,21 +31,21 @@ public class ContactInfo {
 		}
 
 
-		public static Builder<False, False, False> create() {
-			return new Builder<False, False, False>();
-		}
+//		public static Builder<False, False, False> create() {
+//			return new Builder<False, False, False>();
+//		}
 
-		public Builder<True, HasLastName, HasEmail> setFirst(String first) {
+		public Builder<True, HasLastName, HasEmail> first(String first) {
 			this.firstName = first;
 			return (Builder<True, HasLastName, HasEmail>) this;
 		}
 
-		public Builder<HasFirstName, True, HasEmail> setLastName(String lastName) {
+		public Builder<HasFirstName, True, HasEmail> lastName(String lastName) {
 			this.lastName = lastName;
 			return (Builder<HasFirstName, True, HasEmail>) this;
 		}
 
-		public Builder<HasFirstName, HasLastName, True> setEMail(String eMail) {
+		public Builder<HasFirstName, HasLastName, True> eMail(String eMail) {
 			this.eMail = eMail;
 			return (Builder<HasFirstName, HasLastName, True>) this;
 		}
@@ -61,7 +61,7 @@ public class ContactInfo {
 	public void test(){
 		ContactInfo.BuilderWrapper builder = ContactInfo.BuilderWrapper.create();
 
-		ContactInfo info = new ContactInfo(builder.setFirst("A").setLastName("B").setEMail("C"));
+		ContactInfo info = new ContactInfo(builder.first("AAA").eMail("ccc@ccc.cc").lastName("BBB"));
 
 	}
 

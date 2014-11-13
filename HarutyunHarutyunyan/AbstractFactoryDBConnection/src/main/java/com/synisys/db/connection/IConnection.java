024aff894c539211589df68eeabc553b55676167
harutyun.harutyunyan <harutyun.harutyunyan@arm.synisys.com>
2014-11-13 18:25:15 +0400

    Structural Changes

--- HarutyunHarutyunyan/AbstractFactoryDBConnection/src/main/java/com/synisys/db/connection/IConnection.java
+++
@@ -1,9 +0,0 @@
-package com.synisys.db.connection;
-
-public interface IConnection {
-	  /**
-	   * Gets the connection DB vendor name.
-	   * @return
-	   */
-	  public String getVendorName();
-	}
--- HarutyunHarutyunyan/AbstractFactoryDBConnection/src/main/java/com/synisys/db/connection/MySQLConnection.java
+++
@@ -1,9 +0,0 @@
-package com.synisys.db.connection;
-
-public class MySQLConnection implements IConnection {
-
-	
-	public String getVendorName() {
-		return "MySQL";
-	}
-}

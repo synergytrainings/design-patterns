ef92b693d45873c160a08c56102e980fd835c0a0
harutyunharutyunyan <harutyunharutyunyan@users.noreply.github.com>
2014-11-17 19:33:23 +0400

    Update Test.java

--- HarutyunHarutyunyan/StructuralPatterns/BridgePattern/src/main/java/Test.java
+++ HarutyunHarutyunyan/StructuralPatterns/BridgePattern/src/main/java/Test.java
@@ -1,6 +1,3 @@
-
-import java.util.Scanner;
-
 import com.synisys.abstraction.LongMessage;
 import com.synisys.abstraction.Message;
 import com.synisys.abstraction.ShortMessage;
@@ -11,22 +8,12 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		System.out
-				.println("Do you want to send 'long' or 'short' message?");
-		Scanner scanner = new Scanner(System.in);
-		String messageType = scanner.next();
-
-		System.out.println("Please enter the message you want to send");
-		Scanner scanner1 = new Scanner(System.in);
-		String message = scanner1.nextLine();
-
-		if (messageType.equalsIgnoreCase("long")) {
 			Message longMessage = new LongMessage(new EmailMessageSender());
-			longMessage.sendMessage(message);
-		} else {
+			longMessage.sendMessage("Dear bla bla bla");
+		
 			Message shortMessage = new ShortMessage(new SmsMessageSender());
-			shortMessage.sendMessage(message);
-		}
+			shortMessage.sendMessage("SMS");
+		
 
 	}
 }
